c60bde78ca15c8973d071434adddcf36fe574b30
teenrage-dev <renatraiend25@gmail.com>
2023-09-17 14:56:24 +0300

    feat: add filters to image

--- src/components/BackgoundImage.jsx
+++ src/components/BackgoundImage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import myFaceImage from '../assets/images/my-face.png';
-import myFaceImageCartoon from '../assets/images/my-face-cartone-big.png';
 import { ReactComponent as BackgroundFace } from '../assets/images/backgorund-face.svg';
 
 import '../styles/components/_backgroundImage.scss';
